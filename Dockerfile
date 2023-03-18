0929d44eae2460c1de0db62cd5b3931e32809637
Ákos Takács <takacs.akos@it-sziget.hu>
2023-03-18 17:44:34 +0100

    support specific PHP version in php.sh

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-FROM php:7.2-alpine
+ARG PHP_VERSION="7.2"
 
-RUN apk add --no-cache autoconf g++ make \
-    && yes | pecl install xdebug-3.1.6 \
-    && echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini \
-    && echo "xdebug.remote_enable=on" >> /usr/local/etc/php/conf.d/xdebug.ini \
-    && echo "xdebug.remote_autostart=off" >> /usr/local/etc/php/conf.d/xdebug.ini
+FROM php:${PHP_VERSION}-alpine
+
+ARG XDEBUG_VERSION="3.1.6"
+
+# linux-headers is required in php:8.0-alpine
+RUN apk add --no-cache autoconf g++ make linux-headers \
+    && yes | pecl install "xdebug-$XDEBUG_VERSION" \
+    && echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
--- php.sh
+++ php.sh
@@ -2,11 +2,27 @@
 
 set -eu -o pipefail
 
-IMAGE_NAME=rimelek-iputil-test
+IMAGE_NAME_PREFIX=rimelek-iputil-test
 
 CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "$CURR_DIR"
+
+PHP_VERSION="$1"
+shift
+
+IMAGE_NAME="$IMAGE_NAME_PREFIX-php-$PHP_VERSION"
+
+declare -A PHP_XDEBUG_VERSIONS=(
+  [7.2]="3.1.6"
+  [7.3]="3.1.6"
+  [7.4]="3.1.6"
+  [8.0]="3.2.0"
+  [8.1]="3.2.0"
+  [8.2]="3.2.0"
+)
+
+XDEBUG_VERSION="${PHP_XDEBUG_VERSIONS[$PHP_VERSION]}"
 
 ARGS=("$@")
 
@@ -35,7 +51,12 @@ fi
 IMAGE_COUNT=$(docker image list | grep -c "$IMAGE_NAME" || true)
 
 if (( "$IMAGE_COUNT" == 0 )); then
-    docker build --no-cache -t "$IMAGE_NAME" "$CURR_DIR"
+    docker build "$CURR_DIR" \
+      --build-arg "PHP_VERSION=$PHP_VERSION" \
+      --build-arg "XDEBUG_VERSION=$XDEBUG_VERSION" \
+      --no-cache \
+      --tag "$IMAGE_NAME"
+      
     IMAGE_COUNT=$(docker image list | grep -c "$IMAGE_NAME" || true)
     if (( "$IMAGE_COUNT" == 0 )); then
         >&2 echo "The image cannot be built: $IMAGE_NAME";
