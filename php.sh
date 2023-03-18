aa49c01d87b1d1fc342cb9a9c7d2be8500073798
Ákos Takács <takacs.akos@it-sziget.hu>
2023-03-18 02:14:08 +0100

    modernize php.sh

--- php.sh
+++ php.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
 IMAGE_NAME=rimelek-iputil-test
 
-CURR_DIR=$(dirname $(readlink -f $0))
+CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-cd ${CURR_DIR}
+cd "$CURR_DIR"
 
-ARGS=$@
+ARGS=("$@")
 
 declare -A tmp_alternatives=(
   [Darwin]="/private/var/folders/dd"
@@ -15,30 +17,35 @@ declare -A tmp_alternatives=(
 
 tmp_path="${tmp_alternatives[$(uname)]}"
 
-MOUNT_PATHS="-v ${PWD}:${PWD} -v $tmp_path:$tmp_path"
+MOUNT_PATHS=(
+  -v "$PWD:$PWD"
+  -v "$tmp_path:$tmp_path"
+)
 
-if [ "$#" -gt 1 ]; then
-    COVERAGE_CLOVER_ARG=$(echo ${ARGS} | grep -E -o '\-\-coverage-clover ([^ ]+)');
+if (( "$#" > 1 )); then
+    COVERAGE_CLOVER_ARG=$(echo "${ARGS[*]}" | grep -E -o '\-\-coverage-clover ([^ ]+)' || true);
     if [ "${COVERAGE_CLOVER_ARG}" != "" ]; then
-        export COVERAGE_CLOVER_DIR=$(dirname $(echo ${COVERAGE_CLOVER_ARG} | sed 's/--coverage-clover //g'));
-        MOUNT_PATHS=${MOUNT_PATHS}" -v ${COVERAGE_CLOVER_DIR}:${COVERAGE_CLOVER_DIR}"
+        COVERAGE_CLOVER_DIR="$(dirname "${COVERAGE_CLOVER_ARG//--coverage-clover /}")";
+        MOUNT_PATHS+=(-v "$COVERAGE_CLOVER_DIR:$COVERAGE_CLOVER_DIR")
+
+        export COVERAGE_CLOVER_DIR
     fi
 fi
 
-IMAGE_COUNT=$(docker image list | grep ${IMAGE_NAME} | wc -l)
+IMAGE_COUNT=$(docker image list | grep -c "$IMAGE_NAME")
 
-if [ "${IMAGE_COUNT}" -eq 0 ]; then
-    docker build --no-cache -t ${IMAGE_NAME} ${CURR_DIR}
-    IMAGE_COUNT=$(docker image list | grep ${IMAGE_NAME} | wc -l)
-    if [ "${IMAGE_COUNT}" -eq 0 ]; then
-        >&2 echo "The image cannot be built: ${IMAGE_NAME}";
+if (( "$IMAGE_COUNT" == 0 )); then
+    docker build --no-cache -t "$IMAGE_NAME" "$CURR_DIR"
+    IMAGE_COUNT=$(docker image list | grep -c "$IMAGE_NAME")
+    if (( "$IMAGE_COUNT" == 0 )); then
+        >&2 echo "The image cannot be built: $IMAGE_NAME";
         exit 1
     fi
 fi
 
-if [ "${IMAGE_COUNT}" -gt 1 ]; then
-    >&2 echo "There more image name containing the string: ${IMAGE_NAME}";
+if (( "$IMAGE_COUNT" > 1 )); then
+    >&2 echo "There more image name containing the string: $IMAGE_NAME";
     exit 1
 fi
 
-docker run --rm ${MOUNT_PATHS} -u $(id -u):$(id -g) -w ${PWD} ${IMAGE_NAME} php ${ARGS}
+docker run --rm "${MOUNT_PATHS[@]}" -u "$(id -u):$(id -g)" -w "$PWD" "$IMAGE_NAME" php "${ARGS[@]}"
